c058810862133d0e5a1d698cc9d72496ce00d630
Chovy2020 <chovyxu@gmail.com>
2020-03-15 19:47:58 +0800

    config 模块

--- src/pages/Config/index.js
+++ src/pages/Config/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
-import { Row, Col, Button, Tabs, Form, Checkbox, Transfer } from 'antd'
+import {
+  Row,
+  Col,
+  Button,
+  Tabs,
+  Form,
+  Input,
+  Checkbox,
+  Transfer,
+  Table,
+  message,
+  Modal,
+  AutoComplete,
+  Select,
+  List
+} from 'antd'
 // import { delay } from '@/utils/web'
 import { SYSTEM_MODULES, MODEL_LIFECYCLE, ROLES } from './constant'
 import {
@@ -11,9 +26,68 @@ import {
   StyleConfiguration,
   StylePermission,
   StyleTransferModal,
-  StyleFunction
+  StyleFunction,
+  StyleGroupConfigure,
+  StyleGroupTableContainer,
+  StyleGroupTable,
+  StyleHotkeys
 } from './style'
 import Legend from '@/assets/images/legend.png'
+
+const EditableContext = React.createContext()
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+)
+const EditableFormRow = Form.create()(EditableRow)
+class EditableCell extends React.Component {
+  state = {
+    editing: false
+  }
+  toggleEdit = () => {
+    const editing = !this.state.editing
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus()
+      }
+    })
+  }
+  save = e => {
+    const { record, handleSave } = this.props
+    this.form.validateFields((error, values) => {
+      if (error && error[e.currentTarget.id]) {
+        return
+      }
+      this.toggleEdit()
+      handleSave({ ...record, ...values })
+    })
+  }
+  renderCell = form => {
+    this.form = form
+    const { children, dataIndex, record, title } = this.props
+    const { editing } = this.state
+    return editing ? (
+      <Form.Item style={{ margin: 0 }}>
+        {form.getFieldDecorator(dataIndex, { initialValue: record[dataIndex] })(
+          <Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />
+        )}
+      </Form.Item>
+    ) : (
+      <div className='editable-cell-value-wrap' style={{ paddingRight: 24 }} onClick={this.toggleEdit}>
+        {children}
+      </div>
+    )
+  }
+  render() {
+    const { editable, dataIndex, title, record, index, handleSave, children, ...restProps } = this.props
+    return (
+      <td {...restProps}>
+        {editable ? <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer> : children}
+      </td>
+    )
+  }
+}
 
 class Config extends React.Component {
   constructor(props) {
@@ -54,7 +128,53 @@ class Config extends React.Component {
         }
       ],
       selected: [],
-      target: []
+      target: [],
+      groups: [
+        {
+          id: 1,
+          name: 'group 001',
+          data: [
+            {
+              key: 1,
+              product: 'Device1',
+              step: 'StepA'
+            },
+            {
+              key: 2,
+              product: 'Device1',
+              step: 'StepB'
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: 'group 002',
+          data: [
+            {
+              key: 3,
+              product: 'Device1',
+              step: 'StepA'
+            },
+            {
+              key: 4,
+              product: 'Device1',
+              step: 'StepB'
+            }
+          ]
+        }
+      ],
+      activeGroupId: null,
+      // hotkeys
+      visible: false,
+      classCodes: ['0-No_Review', '1-FALSE', '2-Unknown', '278-MG_Replaced', '279-MG_Missing'], // ！通过接口获取
+      classCode: '',
+      hotkeys: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+      hotkey: '',
+      hotkeyMapping: {
+        A: '0-No_Review',
+        B: '1-FALSE',
+        C: '2-Unknown'
+      }
     }
   }
 
@@ -90,10 +210,133 @@ class Config extends React.Component {
     this.setState({ transferVisible: false })
   }
 
+  handleSave = (groupId, row) => {
+    const { groups } = this.state
+    for (const group of groups) {
+      if (group.id === groupId) {
+        for (const i in group.data) {
+          if (group.data[i].key === row.key) {
+            group.data[i] = row
+            break
+          }
+        }
+      }
+    }
+    this.setState({ groups })
+  }
+  handleDelete = rowKey => {
+    console.log(rowKey)
+    const { groups } = this.state
+    for (const i in groups) {
+      for (const j in groups[i].data) {
+        if (groups[i].data[j].key === rowKey) {
+          groups[i].data.splice(j, 1)
+          break
+        }
+      }
+    }
+    this.setState({ groups })
+  }
+  onAddGroup = () => {
+    const { groups } = this.state
+    groups.push({
+      id: Math.floor(Math.random() * 1000000),
+      name: 'group 002',
+      data: [
+        {
+          key: 5,
+          product: 'Device1',
+          step: 'StepA'
+        },
+        {
+          key: 6,
+          product: 'Device1',
+          step: 'StepB'
+        }
+      ]
+    })
+    this.setState({ groups })
+  }
+  onAddGroupTableRow = groupId => {
+    const { groups } = this.state
+    for (const i in groups) {
+      if (groups[i].id === groupId) {
+        groups[i].data.push({
+          key: Math.floor(Math.random() * 1000000),
+          product: '',
+          step: ''
+        })
+      }
+    }
+    this.setState({ groups })
+  }
+  onUpdateGroup = () => {
+    message.success('Update successfully')
+  }
+  onHotkeyOk = () => {
+    this.setState({ visible: false })
+    message.success('Hotkeys save successfully')
+  }
+  onAssignHotkey = () => {
+    const { hotkeyMapping, hotkey, classCode } = this.state
+    hotkeyMapping[hotkey] = classCode
+    this.setState({ hotkeyMapping })
+  }
+  onDeleteHotkey = key => {
+    const { hotkeyMapping } = this.state
+    delete hotkeyMapping[key]
+    this.setState({ hotkeyMapping })
+  }
+
   render() {
     const { activePane } = this.state
     const { modelLifecycle } = this.state
-    const { permission, transferVisible, users, selected, target } = this.state
+    const { permission, transferVisible, users, selected, target, groups, activeGroupId } = this.state
+    const { visible, classCodes, classCode, hotkeys, hotkey, hotkeyMapping } = this.state
+
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell
+      }
+    }
+    const COLUMNS = [
+      {
+        width: '44%',
+        title: 'Product',
+        dataIndex: 'product',
+        editable: true
+      },
+      {
+        width: '44%',
+        title: 'Step',
+        dataIndex: 'step',
+        editable: true
+      },
+      {
+        width: '12%',
+        title: ' ',
+        render: (text, record) => (
+          <Button type='danger' shape='circle' icon='delete' onClick={() => this.handleDelete(record.key)} />
+        )
+      }
+    ]
+    const GROUP_COLUMNS = group =>
+      COLUMNS.map(col => {
+        if (!col.editable) {
+          return col
+        }
+        return {
+          ...col,
+          onCell: record => ({
+            record,
+            editable: col.editable,
+            dataIndex: col.dataIndex,
+            title: col.title,
+            handleSave: row => this.handleSave(group, row)
+          })
+        }
+      })
 
     return (
       <StyleConfig>
@@ -181,13 +424,102 @@ class Config extends React.Component {
                 <h4>Model Function</h4>
                 <Row>
                   {['Training', 'Pi-run', 'Production'].map(item => (
-                    <Col span={6} style={{ border: '1px solid #ddd' }}>
+                    <Col key={item} span={6} style={{ border: '1px solid #ddd' }}>
                       <h5>{item}</h5>
                       <Checkbox.Group options={['Update klarf ADC', 'Update Klarf MB']} />
                     </Col>
                   ))}
                 </Row>
               </StyleFunction>
+              <StyleGroupConfigure>
+                <h4>Model Group Configure</h4>
+                <div style={{ padding: '0 20px' }}>
+                  <Button size='small' type='primary' onClick={this.onAddGroup}>
+                    Add
+                  </Button>
+                  <Button size='small' type='primary' onClick={this.onUpdateGroup}>
+                    Update
+                  </Button>
+                  <Button
+                    size='small'
+                    type='primary'
+                    disabled={activeGroupId === null}
+                    onClick={() => this.setState({ visible: true })}
+                  >
+                    Hotkeys
+                  </Button>
+                </div>
+                <StyleGroupTableContainer>
+                  {groups.map(g => (
+                    <StyleGroupTable
+                      key={g.id}
+                      onClick={() => this.setState({ activeGroupId: g.id })}
+                      className={activeGroupId === g.id ? 'active' : ''}
+                    >
+                      <h4>{g.name}</h4>
+                      <Table
+                        size='small'
+                        pagination={false}
+                        components={components}
+                        rowClassName={() => 'editable-row'}
+                        bordered
+                        dataSource={g.data}
+                        columns={GROUP_COLUMNS(g.id)}
+                      />
+                      <Button
+                        size='small'
+                        shape='circle'
+                        icon='plus'
+                        type='primary'
+                        onClick={() => this.onAddGroupTableRow(g.id)}
+                        style={{ marginTop: 2 }}
+                      ></Button>
+                    </StyleGroupTable>
+                  ))}
+                </StyleGroupTableContainer>
+              </StyleGroupConfigure>
+              <Modal
+                title='Hotkey Setup'
+                visible={visible}
+                onOk={this.onHotkeyOk}
+                onCancel={() => this.setState({ visible: false })}
+              >
+                <StyleHotkeys>
+                  <Select
+                    size='small'
+                    style={{ width: 120 }}
+                    defaultValue={hotkey}
+                    onChange={hotkey => this.setState({ hotkey })}
+                  >
+                    {hotkeys.map(k => (
+                      <Select.Option value={k} key={k}>
+                        {k}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                  <AutoComplete
+                    allowClear
+                    size='small'
+                    filterOption={true}
+                    defaultValue={classCode}
+                    dataSource={classCodes}
+                    style={{ width: 150 }}
+                    onSelect={classCode => this.setState({ classCode })}
+                  />
+                  <Button type='primary' size='small' onClick={this.onAssignHotkey} style={{ marginLeft: 10 }}>
+                    Assign
+                  </Button>
+                </StyleHotkeys>
+                <List
+                  itemLayout='horizontal'
+                  dataSource={Object.keys(hotkeyMapping)}
+                  renderItem={key => (
+                    <List.Item actions={[<a onClick={() => this.onDeleteHotkey(key)}>Delete</a>]}>
+                      {`${key} - ${hotkeyMapping[key]}`}
+                    </List.Item>
+                  )}
+                />
+              </Modal>
             </StyleConfiguration>
           </Tabs.TabPane>
         </Tabs>
--- src/pages/Config/style.js
+++ src/pages/Config/style.js
@@ -136,3 +136,51 @@ export const StyleFunction = styled.div`
     padding: 10px;
   }
 `
+
+export const StyleGroupConfigure = styled.div`
+  border: 1px solid #ddd;
+  margin-top: 20px;
+  padding-bottom: 20px;
+  h4 {
+    padding-left: 20px;
+    margin: 10px 0;
+  }
+`
+export const StyleGroupTableContainer = styled.div`
+  width: 100%;
+  padding: 10px 20px;
+  display: flex;
+  flex-wrap: wrap;
+`
+export const StyleGroupTable = styled.div`
+  width: 31%;
+  margin: 10px 2% 10px 0;
+  flex-wrap: wrap;
+  border: 1px solid #ddd;
+  padding: 2px;
+  &.active {
+    border: 1px solid #f00;
+  }
+  .editable-cell {
+    position: relative;
+  }
+  .editable-cell-value-wrap {
+    padding: 5px 12px;
+    cursor: pointer;
+  }
+  .editable-row:hover .editable-cell-value-wrap {
+    border: 1px solid #d9d9d9;
+    border-radius: 4px;
+    padding: 4px 11px;
+  }
+`
+
+export const StyleHotkeys = styled.div`
+  .ant-list-items {
+    padding: 0 5px;
+    margin-top: 20px;
+  }
+  .ant-list-item {
+    padding: 5px 0;
+  }
+`
